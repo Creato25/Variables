ee238ea815affd1befb0b9dfabf793647015d58d
Creato25 <116253413+Creato25@users.noreply.github.com>
2023-10-27 21:46:08 +0530

    Add files via upload

---
+++ exp1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int var1;
+    cout<<"Enter your Age: ";
+    cin>>var1;
+    string var2;
+    cout<<"Enter your Name: ";
+    cin>>var2;
+    float var3;
+    cout<<"Enter your Surname: ";
+    cin>>var3;
+    char var4;
+    cout<<"Enter your stream: ";
+    cin>>var4;
+    cout<<endl;
+
+
+    cout<<"Age"<<var1<<endl;
+    cout<<"First Name "<<var2<<endl;
+    cout<<"Last Name "<<var3<<endl;
+    cout<<"Stream/Branch "<<var4<<endl;
+    cout<<endl;
+
+// Size of the variable
+    // cout<<"Size of the variables are:"<<endl;
+    // cout<<sizeof(var1)<<endl;
+    // cout<<sizeof(var2)<<endl;
+    // cout<<sizeof(var3)<<endl;
+    // cout<<sizeof(var4)<<endl;
+}
